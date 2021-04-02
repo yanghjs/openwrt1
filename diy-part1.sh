c844b2895fd3dda87cbd64d66a27730331a0653a
yanghjs <81601173+yanghjs@users.noreply.github.com>
2021-04-02 10:12:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,9 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git weifuture https://github.com/hongweifuture/openwrt-packages' feeds.conf.default
+sed -i '$a src-git pwdep https://github.com/hongweifuture/pwdep.git' feeds.conf.default
